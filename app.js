075bf3d8829e5282c3390e84a8267d2848ce5746
Arunkumar032024 <arunkumar032024@gmail.com>
2024-05-16 21:50:14 +0530

    Add api

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const uploadQr = document.querySelector("label"),
 qrInput = document.querySelector("#qr-code");
 
+// async function fetchRequest(formData, file){
+//     // sending post request to qr server api with passing form data as body and getting data from it.
+//     let response = await fetch("http://api.qrserver.com/v1/read-qr-code/", {method: 'POST', body: formData});
+//     console.log(response)
+
+// }
+
+// qrInput.addEventListener("change", e => {
+//     let file = e.target.files[0];   // getting user select file
+//     if(!file) return;
+//     let formData = new formData();  // create a new formData object
+//     formData.append('file', file)   // adding select file to formData
+//     fetchRequest(formData, file);   // invoke fetchRequest() function to get data
+// })
+
+
+async function fetchRequest(formData, file){
+    // sending post request to qr server api with passing form data as body and getting data from it.
+    let response = await fetch("http://api.qrserver.com/v1/read-qr-code/", {method: "POST", body: formData});
+    console.log(response)
+
+}
+
 qrInput.addEventListener("change", e => {
-    let file = e.target.files[0];
-    
+    let file = e.target.files[0];   // getting user select file
+    if(!file) return;
+    let formData = new FormData();  // create a new formData object
+    // formData.append('file', file)   // adding select file to formData
+    fetchRequest(formData, file);   // invoke fetchRequest() function to get data
 })
